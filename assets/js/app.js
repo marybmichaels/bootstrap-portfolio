d26a0d346b0cd51782b8f03ddf650ef1380b8562
MaryB <marybmichaels@outlook.com>
2019-02-02 17:48:55 -0600

    train project added

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.crystal').on("click", function() {
+        window.open("https://marybmichaels.github.io/unit-4-game/", "_self");
+    });
+    $('.train-scheduler').on("click", function() {
+        window.open("https://marybmichaels.github.io/train-scheduler/", "_self");
+    });
+    $('.trivia').on("click", function() {
+        window.open("https://marybmichaels.github.io/TriviaGame/", "_self");
+    });
+    $('.giphy').on("click", function() {
+        window.open("https://marybmichaels.github.io/giftastic/", "_self");
+    });
+    // $('.sports').on("click", function() {
+    //     window.open("https://marybmichaels.github.io/.../", "_self");
+    // })
+});
